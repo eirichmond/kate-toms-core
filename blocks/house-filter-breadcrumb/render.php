bccddc1e699c965880fa73ff8089b05bdb968dce
Elliott Richmond <elliott@squareonemd.co.uk>
2026-06-18 14:47:17 +0100

    reset change

--- blocks/house-filter-breadcrumb/render.php
+++ blocks/house-filter-breadcrumb/render.php
@@ -113,6 +113,6 @@ wp_interactivity_state(
 		<svg class="house-filter-breadcrumb__reset-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1920 1920" width="16" height="16" aria-hidden="true" focusable="false">
 			<path d="M960 0v213.333c411.627 0 746.667 334.934 746.667 746.667S1371.627 1706.667 960 1706.667 213.333 1371.733 213.333 960c0-197.013 78.4-382.507 213.334-520.747v254.08H640V106.667H53.333V320h191.04C88.64 494.08 0 720.96 0 960c0 529.28 430.613 960 960 960s960-430.72 960-960S1489.387 0 960 0" fill-rule="evenodd"/>
 		</svg>
-		<span><?php esc_html_e( 'Reset', 'kate-toms-core' ); ?></span>
+		<span><?php esc_html_e( 'Clear filters', 'kate-toms-core' ); ?></span>
 	</button>
 </div>
